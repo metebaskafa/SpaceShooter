8dbcf802cfa006ce2d9bc03e23697d653b39521c
mete başkafa <114742146+metebaskafa@users.noreply.github.com>
2023-10-01 22:32:46 +0300

    init
    First

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    [SerializeField] GameManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3 (0, speed, 0)*Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Player") 
+        {
+            GameManager.instance.InitiateGameOver();
+        }
+        else
+        {
+            GameManager.instance.IncreaseScore(10);
+        }
+        
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+}
